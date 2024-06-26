2d81f360a8f38af6a6ed928c55bf43510b266391
BeckieMorton <beckam11@gmail.com>
2024-06-26 12:27:00 +1000

    fetch works but need to note that i have to determin the continent as that is in the url so need to fix that atm it works for europe

--- src/components/CountryInfo/Details.jsx
+++ src/components/CountryInfo/Details.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Details({ gecCode }) {
-  //fetch info
+  // https://raw.githubusercontent.com/factbook/factbook.json/master/europe/gm.json
 
+  const code = { gecCode };
+
+  //fetch info from API
+
+  const [countryDetails, setCountryDetails] = useState("");
+
+  useEffect(() => {
+    const fetchCountryDetails = async () => {
+      const countryAPI = `https://raw.githubusercontent.com/factbook/factbook.json/master/europe/${code}.json`;
+
+      try {
+        const response = await fetch(countryAPI);
+
+        if (response.ok) {
+          const json = await response.json();
+          setCountryDetails(json);
+        } else {
+          console.log(`Invalid response for country gec code ${gecCode}`);
+        }
+      } catch (error) {
+        console.log("Error fetching country data:", error);
+      }
+      console.log(`this is in country details:`, countryDetails);
+    };
+
+    fetchCountryDetails();
+  }, [gecCode]);
   return (
     <div>
       <h3>Details</h3>
